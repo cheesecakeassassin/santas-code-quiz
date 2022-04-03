85052cfac23c974b80a3adfbd6ba140107d1f9ab
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-02 23:12:00 -0400

    initial commit, a bit late for it since I'm rushing to get done

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
+];
+
+// Method that starts the quiz
+function startQuiz() {
+
+    // Hide start screen
+    titleScreen.setAttribute("class", "hide");
+  
+    // Unhide questions section
+    quizScreen.setAttribute("class", "show");
+  
+    // Start timer
+    timerId = setInterval(tick, 1000);
+  
+    // Show starting time
+    timeEl.textContent = time;
+  
+    getQuestion();
+  }
